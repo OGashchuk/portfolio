5c57b4941d485cde5686c6029046fa3d1252a338
Olexandr Gashchuk <sanyok1753@gmail.com>
2024-03-26 16:21:32 +0200

    Add files via upload
    Signed-off-by: Olexandr Gashchuk <sanyok1753@gmail.com>

---
+++ js/main.js
@@ -0,0 +1,58 @@
+$(document).ready(function(){
+    $('a[href^="#"]').click(function (){
+        var elementClick = $(this).attr("href");
+        var destination = $(elementClick).offset().top;
+        jQuery("html:not(:animated), body:not(:animated)").animate({scrollTop: destination}, 800);
+        return false;
+    })
+  
+$('.product').slick({
+  dots: true,
+  infinite: true,
+  speed: 200,
+  fade: true,
+  cssEase: 'linear',
+    arrows: false,
+    autoplay:true
+});
+   
+$('.reviews').slick({
+  dots: false,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  responsive: [
+    {
+      breakpoint: 976,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 659,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
+});    
+
+    $(".fancybox-thumb").fancybox({
+		prevEffect	: 'none',
+		nextEffect	: 'none',
+		helpers	: {
+			title	: {
+				type: 'outside'
+			},
+			thumbs	: {
+				width	: 50,
+				height	: 50
+			}
+		}
+	});
+    
+});
+
